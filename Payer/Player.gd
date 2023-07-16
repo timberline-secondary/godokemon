de14d66c3756b5c8289ae50f0a4a7650f6c8f645
TimberlineHackerspace <timberline.hackerspace@gmail.com>
2023-07-15 19:45:12 -0700

    Quest 2 completed

---
+++ Payer/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const SPEED := 100.0
+onready var anim = $AnimationTree
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down", 0)
+	var velocity = Vector2.ZERO
+	if input_dir:
+		velocity = input_dir * SPEED
+		anim.get("parameters/playback").travel("Walk")
+		anim.set("parameters/Idle/blend_position", input_dir)
+		anim.set("parameters/Walk/blend_position", input_dir)
+		
+	else:
+		anim.get("parameters/playback").travel("Idle")
+		velocity.x = move_toward(velocity.x, 0, 20)
+		velocity.y = move_toward(velocity.y, 0, 20)
+	
+	
+	
+	move_and_slide(velocity)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
